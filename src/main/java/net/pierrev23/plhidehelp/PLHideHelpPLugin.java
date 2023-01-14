5711f8f5e144ee1dc67a7abb6ae8255d34059e4d
Pièrre <77776198+PierreV23@users.noreply.github.com>
2023-01-14 01:01:50 +0100

    start

--- src/main/java/net/pierrev23/plhidehelp/PLHideHelpPLugin.java
+++
@@ -1,6 +0,0 @@
-package net.pierrev23.plhidehelp;
-
-import org.bukkit.plugin.java.JavaPlugin;
-
-public class PLHideHelpPLugin extends JavaPlugin {
-}
---
+++ src/main/java/net/pierrev23/plhidehelp/PLHideHelpPlugin.java
@@ -0,0 +1,18 @@
+package net.pierrev23.plhidehelp;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.plugin.java.JavaPlugin;
+
+
+public class PLHideHelpPlugin extends JavaPlugin {
+    @Override
+    public void onEnable() {
+        Bukkit.getLogger().info(ChatColor.GREEN + "[PLHideHelpPlugin] Enabled " + this.getName());
+    }
+    @Override
+    public void onDisable() {
+        Bukkit.getLogger().info(ChatColor.RED + "[PLHideHelpPlugin] Disabled " + this.getName());
+    }
+
+}
